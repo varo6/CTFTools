5c6ad02050edad94f17553bb4000094cc0daaebd
varo6 <alvarohr2003@gmail.com>
2025-09-04 13:49:29 +0200

    add lazyvim installer

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -4,3 +4,15 @@ sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >>~/.bashrc
 source ~/.bashrc
 rm nvim-linux-x86_64.tar.gz
+
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
+git clone https://github.com/LazyVim/starter ~/.config/nvim
+
+rm -rf ~/.config/nvim/.git
