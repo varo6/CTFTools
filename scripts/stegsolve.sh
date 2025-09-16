82f43c3b281dd68cb81047d5989d9a4456c7a08f
varo6 <alvarohr2003@gmail.com>
2025-09-16 19:05:39 +0200

    fix: relative downloads

--- scripts/stegsolve.sh
+++ scripts/stegsolve.sh
@@ -1,4 +1,4 @@
 sudo apt install openjdk-11-jre -y
 mkdir -p ~/ejecutablesCTF/
 wget http://www.caesum.com/handbook/Stegsolve.jar -O ~/ejecutablesCTF/stegsolve.jar
-chmod +x stegsolve.jar
+chmod +x ~/ejecutablesCTF/stegsolve.jar
